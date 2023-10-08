06e67074b42a57b74b7e048d527ab47ec90e5e97
nhizz <141527429+nhizz@users.noreply.github.com>
2023-10-08 13:58:47 -0500

    Update blade.lua

--- blade.lua
+++ blade.lua
@@ -1,18 +1,9 @@
-local Debug = false 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+```local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
-local RunService = game:GetService("RunService") 
 
 local Player = Players.LocalPlayer or Players.PlayerAdded:Wait()
-local Remotes = ReplicatedStorage:WaitForChild("Remotes", 9e9)
-local Balls = workspace:WaitForChild("Balls", 9e9)
-
-local Threshold = 10 
-local function print(...)
-    if Debug then
-        warn(...)
-    end
-end
+local Remotes = ReplicatedStorage:WaitForChild("Remotes", math.huge)
+local Balls = workspace:WaitForChild("Balls", math.huge)
 
 local function VerifyBall(Ball)
     if typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true then
@@ -28,40 +19,33 @@ local function Parry()
     Remotes:WaitForChild("ParryButtonPress"):Fire()
 end
 
-local pingValue = game.Stats.Network.ServerStatsItem["Data Ping"]:GetValue() / 1000 -- Calculate ping
-
 Balls.ChildAdded:Connect(function(Ball)
     if not VerifyBall(Ball) then
         return
     end
-    
+
     print("Ball Spawned:", Ball)
-    
+
     local OldPosition = Ball.Position
     local OldTick = tick()
-    
+
     Ball:GetPropertyChangedSignal("Position"):Connect(function()
         if IsTarget() then
             local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
             local Velocity = (OldPosition - Ball.Position).Magnitude
-            
-            -- Adjust for ping
-            local pingAcc = Velocity * pingValue
-            
+
             print("Distance:", Distance)
             print("Velocity:", Velocity)
-            
-            if (Distance / (Velocity - pingAcc)) <= Threshold then
+            print("Time:", Distance / Velocity)
+
+            if (Distance / Velocity) <= 10 then
                 Parry()
             end
         end
-        
+
         if (tick() - OldTick >= 1/60) then
             OldTick = tick()
             OldPosition = Ball.Position
         end
     end)
-end)
-
-RunService.Heartbeat:Connect(function()
-end)
+end)```
